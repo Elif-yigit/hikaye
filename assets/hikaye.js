ba1786f30d96fa6198804152d962814140495942
ELIF YIGIT <elifygit@outlook.com>
2024-10-11 21:49:51 +0300

    first commit

---
+++ assets/hikaye.js
@@ -0,0 +1,191 @@
+let secim1 = prompt('Bir gün, deniz kenarında yürürken ufukta eski bir haritada adı geçen, kayıp bir adayı fark ettin. Bu ada, yıllar önce ortadan kaybolan bir medeniyetin efsanelerinde geçiyor. Kıyıda terkedilmiş bir tekne var ve adaya ulaşmanın tek yolu o.Adaya gitmeye karar verdin mi? EVET / HAYIR');
+
+if (secim1 === 'EVET') {
+
+    let secim2 = prompt('Tekneyle adaya vardın. Adanın kıyısında, yıkılmış eski bir kule ve içi karanlık görünen yoğun bir orman var. Ancak hava hızla kararmaya başlıyor.Nereye gideceksin? KULE / ORMAN');
+ 
+ 
+
+if (secim2 === 'KULE') {
+
+    let secim3 = prompt('Kuleye vardığında, zayıf ve dökülmekte olan eski bir merdiven buluyorsun. Merdiven yukarı, zirveye çıkıyor. Ancak kulenin duvarlarında eski, bilinmeyen bir dilde yazılar ve semboller var.Merdivene tırmanacak mısın? EVET / HAYIR');
+
+
+if (secim3 === 'EVET') {
+
+    let secim4 = prompt('Merdiveni tırmanıyorsun ve en üst kata ulaştığında büyük bir odanın kapısını buluyorsun. Odanın içinde bir masa var ve masanın üzerinde bir harita duruyor.Haritayı inceleyecek misin? EVET/HAYIR');
+
+if (secim4 ==='EVET') {
+
+    let secim5 = prompt('Haritayı dikkatlice inceliyorsun. Harita, adanın diğer bölgelerini ve kayıp hazineyi bulmak için önemli olan yerleri gösteriyor. Harita üzerinde bir X işareti var; bu işaret hazinenin yerini gösteriyor. Macerana devam etme kararı alıyorsun.Haritada başka bir hazine işareti var. Peşine düşecek misin? EVET/HAYIR');
+
+ if ( secim5 === 'EVET') {
+
+    let secim6 = prompt('Haritadaki hazine işaretinin peşine düşüyorsun. Haritanın talimatlarını takip ederek adanın derinliklerine doğru ilerliyorsun. Bu seçim seni yeni maceralara götürüyor. Odayı keşfederken duvarda bir kapı buldun. Geçecek misin? EVET/HAYIR');
+ if  (secim6 === 'EVET') {
+
+    let secim7 = prompt('Duvardaki kapıyı açıyorsun ve içeride karanlık bir odanın olduğunu görüyorsun. Bu odada eski kalıntılar ve belki de kayıp medeniyetin sırları olabilir.Odayı terk etmeden önce masayı kontrol edecek misin? EVET/HAYIR');
+
+ if (secim7 === 'EVET') {
+
+    alert('masayı kontrol ettiğinde, karşına tozlu sayfalarıyla eski bir günlük çıkıyor. Bu günlük, kaybolmuş medeniyetin tarihi ve adada saklanan hazineye dair gizli sırlar barındırıyor. Bilgileri dikkatle inceleyerek adanın derinliklerine doğru yola çıkmaya karar veriyorsun. Nihayet, hazinenin bulunduğu yere ulaştığında, karşında büyüleyici bir manzara açılıyor: Altınlar, mücevherler ve kayıp medeniyetin eserleri seni bekliyor! Kayıp hazinenin sırlarını çözdüğünde, adanın gerçek koruyucusu oluyorsun. Artık, bu sırlarla dolu ada, senin cesaretin ve keşiflerin sayesinde tekrar canlanıyor. Efsaneler, artık senin hikayenle birleşiyor ve adanın destanı sonsuza kadar seninle yaşıyor!');
+
+ } else {
+    alert('Odayı terk ediyorsun, ancak dışarıda seni bekleyen yeni maceralar var. Hala sırlarla dolu bu adanın keşfine devam edebilirsin.');
+}
+
+
+}
+}
+}
+}
+}
+}
+else if( secim1 === 'HAYIR') {
+
+    let secimHayir2 = prompt('Adaya gitmemeye karar veriyorsun ve sahilde kalıyorsun. Bir süre sahilde yürümeye devam ediyorsun. Yürürken denizden gelen bir fırtına belirmeye başlıyor.Fırtına yaklaşıyor. Güvenli bir yere sığınmaya karar verir misin? EVET / HAYIR');
+ if(secimHayir2 = 'EVET') {
+
+let secimHayirEvet = prompt ('Mağaraya sığınıyorsun, içeride güvendesin. Fırtına dışarıda şiddetle devam ederken, mağaranın içinde eski kalıntılar ve yazılar görüyorsun. Bu yazıların kayıp bir medeniyete ait olduğunu fark ediyorsun. İçeride ilerlemeye devam edip etmeyeceğine karar vermen gerekiyor.Mağaranın derinliklerine ilerlemeye karar verir misin? EVET / HAYIR');
+ if(secimHayirEvet === 'EVET') {
+
+let secimHayirEvetEvet = prompt('Haritayı takip ederek mağaradan çıkıyor ve adanın derinliklerine gidiyorsun. Bu yolda eski tuzaklar var. Yola devam eder misin? EVET / HAYIR');
+
+if(secimHayirEvetEvet === 'EVET') {
+
+let secimHayirEvetEvetEvet = prompt('Hazinenin bulunduğu odaya girdiğinde, kapıyı açacak mısın? EVET / HAYIR');
+
+ if (secimHayirEvetEvetEvet === 'EVET') {
+
+let secimHayirEvetEvetEvetEvetEvet = prompt('Hazinenin gerçek koruyucusu olmaya karar verip adada kalacak mısın? EVET / HAYIR');
+{  if (secimHayirEvetEvetEvetEvetEvet === 'EVET') 
+
+    alert('Adada kalmaya ve hazinenin gerçek koruyucusu olmaya karar veriyorsun. Bu, sadece büyük bir sorumluluk değil, aynı zamanda efsanelerde adı geçen büyük bir onur. Artık kayıp medeniyetin sırları senin ellerinde. Bu adanın derinliklerinde, kimsenin ulaşamayacağı bir yerde, hazinenin ve medeniyetin gizemlerini koruyacaksın. Zaman geçtikçe, ada senin evin, hazine ise koruyacağın bir emanet haline geliyor.Yıllar sonra bile, adanın koruyucusu olarak, efsaneler seni anlatmaya devam edecek. Bu kayıp ada, senin cesaretin ve keşiflerin sayesinde tekrar hayat buldu. Senin hikayen, adanın yeni destanı olarak sonsuza kadar yaşayacak.');
+}
+}
+}
+}
+}
+}
+
+
+else if( secimHayir2 === 'HAYIR') { 
+
+    let secimHayirHayir = prompt('Sığınacak yer bulamayıp, sahile daha yakın bir bölgede kalmayı tercih eder misin? EVET/HAYIR ');
+    
+    if(secimHayirHayir === 'HAYIR') {
+
+        let secimHayirHayirHayir = prompt ('Sahilde kalmamaya karar verdin. Ancak etrafına bakarken başka bir güvenli yer de bulamıyorsun. Sahile geri dönmek ister misin? EVET/HAYIR');
+
+        if (secimHayirHayirHayir === 'HAYIR') {
+
+           let secimHayirHayirHayirHayir = prompt('Sahile geri dönmemeye karar verdin. Fırtına yaklaşıyor ve etrafta hala güvenli bir yer bulamıyorsun. Rüzgar şiddetle esmeye başladı. Bir mağara veya sığınacak bir yer bulamazsan fırtına seni yakalayacak. Yine de dışarıda kalmaya devam mı edersin?Etrafta başka bir güvenli yer aramaya devam eder misin? EVET / HAYIR');
+
+    if (secimHayirHayirHayirHayir === 'HAYIR') {
+
+let secimHayir7 = prompt('Dışarıda kalmaya karar verdin ama fırtına giderek güçleniyor. Ne yapacaksın? Sırtına rüzgâra karşı çıkmayı mı yoksa yakınındaki ağaçların altına sığınmayı mı tercih edeceksin? (AĞAÇ ALTINA / DUR)"');
+
+if (secimHayir7 === 'AĞAÇ ALTINA') {
+
+    let secimHayir8 = prompt('Ağaçların altına sığınmaya karar veriyorsun. Ancak fırtına sırasında düşen bir dal seni yaralıyor. Hemen oradan uzaklaşıp başka bir yer arayacak mısın? (EVET / HAYIR)');
+if (secimHayir8 ===  'HAYIR') {
+
+    let secim9 = prompt('Dışarıda kalmaya devam ediyorsun. Fırtınanın giderek şiddetlendiğini hissediyorsun. Fırtınadan korunmak için vücudunu sarmalayacak mısın? EVET / HAYIR');
+
+   if (secim9 === 'HAYIR') { 
+
+let secim10 = prompt ('Sarmamayı tercih ediyorsun. Fırtına esnasında etrafındaki seslere dikkat ediyor musun?'); {
+
+    if (secim10 === 'HAYIR') {
+
+        let secim11 = prompt ('Seslere dikkat etmemeye karar veriyorsun. Fırtınanın dinmesini beklemenin riskli olduğunu düşünüyor musun? EVET / HAYIR');
+        
+        if (secim11 === 'HAYIR') {
+
+let secim12 = prompt('Beklemeyi tercih ediyorsun. Fırtına devam ederken, karşına bir yol ayrımı çıkıyor. Hangi yolu seçeceksin? DÜMDÜZ / SARP YOL');
+
+
+if (secim12 === 'DÜMDÜZ') {
+
+ alert ('Düz yoldan geçmeyi seçiyorsun. Ancak aniden bir çukura düşüyorsun ve orada mahsur kalıyorsun. Fırtına seni buluyor ve sonunda ıslanıyorsun. Yardım çağırmak zorunda kalıyorsun. Fırtınadan dolayı hiçbir ses duymuyorsun. Sonunda, çukurdan çıkmak için çabalıyorsun ama başarısız oluyorsun. Fırtına seni yakalıyor.');
+}
+
+
+if (secim12 === 'SARP YOL') {
+
+alert ('Sarp yolda ilerlemeye karar veriyorsun. Yol çok kaygan ve zorlu. Bir anda dengenizi kaybedip düşüyorsun. Düşerken kafanı çarpıyor ve bayılıyorsun. Fırtına seni bulduğunda, dışarıda kalıyorsun ve bu da sonunu getiriyor.');
+
+
+        
+}
+
+}
+}
+}
+   }
+
+}
+}
+}
+}
+    }
+    }
+
+
+    else if( secimHayir2 === 'HAYIR') { 
+
+        let secimHayirHayir = prompt('Sığınacak yer bulamayıp, sahile daha yakın bir bölgede kalmayı tercih eder misin? EVET/HAYIR ');
+        
+        if(secimHayirHayir === 'EVET') {
+
+            letsecim21 = prompt('Sığınacak yer bulamayıp, sahile daha yakın kalmayı tercih ediyorsun. Ancak fırtına yaklaşıyor ve hava hızla kötüleşiyor. Ne yapacaksın?Sahile dönmeye karar verip, teknenin yanına gitmek ister misin? (EVET / HAYIR)');
+
+            if (secim21 === 'EVET') {
+
+             letsecim22 = prompt ('Teknenin yanına dönüyorsun, ama fırtına patlak veriyor. Tekneyle kaçmaya çalışırken, dalgalar seni engelliyor. Sonunda, tekne devrilip suda kayboluyorsun. Fırtına seni yakalıyor.Fırtınadan kaçmak için yakınlarda bir mağara bulmayı dener misin? EVET / HAYIR');
+
+if (secim21 === 'HAYIR')  {
+
+    letsecim23 = prompt('Sahilde kalmaya devam ediyorsun. Ama fırtına seni buluyor ve şiddetli rüzgar seni sürüklüyor. Şansın kalmıyor ve fırtına seni yakalıyor.Fırtınadan kaçmak için yakınlarda bir mağara bulmayı dener misin? EVET / HAYIR');
+
+if (secim22 === 'EVET') {
+
+letsecim24 = prompt ('Yakındaki bir mağaraya doğru koşuyorsun. İçeri girdiğinde, fırtınadan korunmuş oluyorsun. Ancak, mağaranın derinliklerinde eski kalıntılar buluyorsun. Devam etmeye karar verirsen, kaybolma riski var. İlerlemeye devam edecek misin? (EVET / HAYIR)');
+
+if(secim24 === 'EVET') {
+
+    letsecim25 = prompt('Etrafta başka bir güvenli yer aramaya devam mı edersin? (EVET / HAYIR)');
+
+    if (secim24 === 'HAYIR') {
+
+letsecim26 = prompt ('Mağaraya girmemeye karar veriyorsun. Ancak dışarıda fırtına devam ediyor. Bir süre daha beklemen gerekiyor ama sonunda fırtına seni yakalıyor.Etrafta başka bir güvenli yer aramaya devam mı edersin? (EVET / HAYIR)');
+
+if ( secim26 === 'EVET') {
+
+let secim27 = prompt('Etrafta güvenli bir yer arıyorsun ama zaman daralıyor. Fırtına artık daha da yakın. Güvenli bir yer bulamazsan, dışarıda kalmanın sonuçlarına katlanacaksın. Bir grup insan bulmayı dener misin? (EVET / HAYIR)');
+if (secim27 === 'EVET') {
+
+
+    let secim28 = prompt ('Yakınlarda bir grup insan buluyorsun. Birlikte güvenli bir yere sığınmaya çalışıyorsunuz. Ancak, fırtına onları da etkiliyor. Yine de birlikte daha güçlü duruyorsunuz.Fırtınayı görmezden gelip yürüyüşe devam etmeye karar veriyor musun? (EVET / HAYIR)');
+
+if (secim28 === 'HAYIR') {
+
+    alert ('Fırtınayı umursamadan yola devam ediyorsun. Ancak aniden rüzgar seni yere düşürüyor. Fırtına seni yakalıyor. (KAYBETTİN)');
+
+if (secim28 === 'EVET') {
+
+    alert('Fırtınanın getirdiği tehlikeleri fark edip sığınacak bir yer arıyorsun ama bulamıyorsun. Rüzgar giderek şiddetleniyor ve tam o anda sığ sularda bir ışık görüyorsun. Işığın kaynağına doğru koşmaya başlıyorsun ve terkedilmiş bir kabine giriyorsun. Sonuç: Kabin, kayıp medeniyetin izlerini taşıyor. Fırtına seni yakalayamadı, şimdi keşfetme zamanı!');
+}
+
+}
+
+}
+
+}
+}
+}
+}
+}
+}
+}
+}
